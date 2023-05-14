8bd3c8af3269d6a3d9f4d1c42c9f44acf3ef0826
Mr-8k <emildimitrov97@yahoo.com>
2023-05-14 05:41:20 +0300

    Initial

--- src/data/scripts/plugins/RemoveHyperspaceGarbagePlugin.java
+++ src/data/scripts/plugins/RemoveHyperspaceGarbagePlugin.java
@@ -2,6 +2,7 @@ package data.scripts.plugins;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignEventListener;
 import com.fs.starfarer.api.campaign.CampaignTerrainAPI;
 import com.fs.starfarer.api.campaign.LocationAPI;
 import com.fs.starfarer.api.campaign.SectorAPI;
@@ -9,6 +10,7 @@ import com.fs.starfarer.api.impl.campaign.ghosts.SensorGhostManager;
 import com.fs.starfarer.api.impl.campaign.ids.Terrain;
 import com.fs.starfarer.api.impl.campaign.velfield.SlipstreamManager;
 import org.apache.log4j.Logger;
+import scripts.RHSG_Hostility_Remover;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,28 +26,50 @@ public class RemoveHyperspaceGarbagePlugin extends BaseModPlugin {
 
         SectorAPI sector = Global.getSector();
 
-        if (sector.hasScript(SlipstreamManager.class)) {
-            sector.removeScriptsOfClass(SlipstreamManager.class);
-            log.info("Removed slipstream manager.");
+        if(Global.getSettings().getBoolean("RHSG_removeSlipstreams")) {
+            if (sector.hasScript(SlipstreamManager.class)) {
+                sector.removeScriptsOfClass(SlipstreamManager.class);
+                log.info("Removed slipstream manager.");
+            }
+            LocationAPI hyperspace = Global.getSector().getHyperspace();
+            List<CampaignTerrainAPI> allTerrain = Global.getSector().getHyperspace().getTerrainCopy();
+            for (CampaignTerrainAPI terrain : allTerrain) {
+                if (Objects.equals(terrain.getType(), Terrain.SLIPSTREAM)) {
+                    hyperspace.removeEntity(terrain);
+                    log.info("Removed slipstream at: " + terrain.getLocationInHyperspace());
+                }
+            }
         }
 
         //boolean playerInHyperspace = Global.getSector().getPlayerFleet().isInHyperspace();
 
         //if (!playerInHyperspace) {
-        if (sector.hasScript(SensorGhostManager.class)) {
-            sector.removeScriptsOfClass(SensorGhostManager.class);
-            Global.getSector().getMemoryWithoutUpdate().unset("$ghostManager");
-            log.info("Removed ghost manager.");
-        }
+        if(Global.getSettings().getBoolean("RHSG_removeGhosts"))
+            if (sector.hasScript(SensorGhostManager.class)) {
+                sector.removeScriptsOfClass(SensorGhostManager.class);
+                Global.getSector().getMemoryWithoutUpdate().unset("$ghostManager");
+                log.info("Removed ghost manager.");
+            }
         //} else log.info("Ghost manager not removed, please load game outside of hyperspace.");
 
-        LocationAPI hyperspace = Global.getSector().getHyperspace();
-        List<CampaignTerrainAPI> allTerrain = Global.getSector().getHyperspace().getTerrainCopy();
-        for (CampaignTerrainAPI terrain: allTerrain) {
-            if (Objects.equals(terrain.getType(), Terrain.SLIPSTREAM)){
-                hyperspace.removeEntity(terrain);
-                log.info("Removed slipstream at: " + terrain.getLocationInHyperspace());
+/*        if(Global.getSettings().getBoolean("RHSG_removeHyperspaceTerrain")) {
+            LocationAPI hyperspace = Global.getSector().getHyperspace();
+            List<CampaignTerrainAPI> allTerrain = Global.getSector().getHyperspace().getTerrainCopy();
+            for (CampaignTerrainAPI terrain : allTerrain) {
+                if (Objects.equals(terrain.getType(), Terrain.HYPERSPACE)) {
+                    hyperspace.removeEntity(terrain);
+                    log.info("Removed hyperspace at: " + terrain.getLocationInHyperspace());
+                }
             }
+        }*/
+
+        if(Global.getSettings().getBoolean("RHSG_removeHostileActivityProgress")) {
+            sector = Global.getSector();
+            if (!sector.getListenerManager().hasListenerOfClass(RHSG_Hostility_Remover.class))
+                sector.getListenerManager().addListener(new RHSG_Hostility_Remover());
+        } else {
+            if (sector.getListenerManager().hasListenerOfClass(RHSG_Hostility_Remover.class))
+                sector.getListenerManager().removeListenerOfClass(RHSG_Hostility_Remover.class);
         }
 
     }
